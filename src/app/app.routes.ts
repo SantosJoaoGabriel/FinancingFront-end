3c6dd7001bee3ef264e47887d4fbfd33acbfbe4d
Jaocomsono <joaopptopgg@gmail.com>
2026-04-13 22:17:12 -0300

    Feat:AdicionandoRotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard';
 import { TransacoesComponent } from './transacoes/transacoes';
+import { GanhosComponent } from './ganhos/ganhos';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'transacoes', component: TransacoesComponent },
+  { path: 'ganhos', component: GanhosComponent },
 ];
