bce198768e3e8f610300825a6758004deb5cd486
Jaocomsono <joaopptopgg@gmail.com>
2026-04-23 19:47:03 -0300

    Feat:AdicionadoRotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard';
 import { TransacoesComponent } from './transacoes/transacoes';
 import { GanhosComponent } from './ganhos/ganhos';
+import { ReportsComponent } from './reports/reports';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'transacoes', component: TransacoesComponent },
   { path: 'ganhos', component: GanhosComponent },
+  { path: 'reports', component: ReportsComponent }
 ];
